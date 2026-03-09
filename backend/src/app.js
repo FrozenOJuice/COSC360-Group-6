80436b46d63e6ec8b2b2ab1f2ffb4bd1aea717cd
FrozenOJuice <79174905+FrozenOJuice@users.noreply.github.com>
2026-03-09 12:47:46 -0700

    feat: add login route

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,10 @@ import { errorHandler } from "./middleware/errorHandler.js";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 app.use(cookieParser());
@@ -21,19 +22,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRouter);
-
-app.post("/submit", (req, res) => {
-    const { name, role } = req.body;
-
-    res.json({
-        message: `Received ${name} with role ${role}`
-    });
-});
-
-app.options("/login", cors());
-
-// Middleware to parse JSON
-app.use(express.json());
 
 app.post("/login", (req,res)=>{
     console.log("HIT /login", req.body);
