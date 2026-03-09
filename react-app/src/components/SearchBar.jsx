45fe2e6535decee11f668e4cb8ed3956b66ae9e0
Eric Harrison <hericeh72@gmail.com>
2026-03-09 12:58:48 -0700

    Added handleSubmit function to searchBar

--- react-app/src/components/SearchBar.jsx
+++ react-app/src/components/SearchBar.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react"; //component can store and update state values
+
 function SearchBar() {
+  const [searchData, setSearchData] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    // request sent to server
+    // await pauses this function until the server responds.
+    const response = await fetch("http://localhost:3000/search", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ //convert form data into JSON text
+        searchData: searchData,
+      })
+    }); 
+
+    const data = await response.json();//await pauses until response conversion to 
+  }
+
   return (
     <div style={{ margin: "20px 0" }}>
       <input
